104ead8e50cec018ad1e1fc67dd729c8f4151f40
azaw502 <andrew502502@gmail.com>
2019-11-03 21:21:47 -0500

    heroku post build

--- src/app.js
+++ src/app.js
@@ -3,16 +3,14 @@ const app = express();
 const path = require('path');
 
 app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, '../../frontend')));
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname + '../../frontend'));
-// });
 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
-});
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port);
