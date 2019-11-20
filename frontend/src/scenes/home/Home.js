ed7daef5fde52c5a2d7dc80b9431a67eb94d5787
azaw502 <andrew502502@gmail.com>
2019-11-19 23:23:02 -0500

    Home.js

--- frontend/src/scenes/home/Home.js
+++ frontend/src/scenes/home/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Typography, Card } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
-import { HomeForm } from './containers';
 import axios from 'axios';
 
 const styles = theme => ({
@@ -19,44 +18,11 @@ const styles = theme => ({
 });
 
 class Home extends Component {
-  state = {
-    artists: []
-  };
-
-  async componentDidMount() {
-    const response = await axios.get('/api/artists');
-    this.setState({
-      artists: response.data.artists
-    });
-  }
-
-  handleSubmit(artist) {
-    this.setState(prevState => ({ artists: [...prevState.artists, artist] }));
-  }
+  state = {};
 
   render() {
     const { classes } = this.props;
-    return (
-      <Container>
-        Note to grader: Other form is found by pressing Login. Return to
-        homepage by pressing "ReleaseM" on left side.
-        <HomeForm handleSubmit={this.handleSubmit.bind(this)} />
-        {this.state.artists ? (
-          this.state.artists.map(artist => (
-            <Card className={classes.card}>
-              <Typography variant="h5">Artist Name: </Typography>
-              {artist.artistName}
-              <Typography variant="h5">Song: </Typography>
-              {artist.songName}
-            </Card>
-          ))
-        ) : (
-          <Card className={classes.card}>
-            <Typography variant="h5">Nothing :(</Typography>
-          </Card>
-        )}
-      </Container>
-    );
+    return <Container>Homepage</Container>;
   }
 }
 
