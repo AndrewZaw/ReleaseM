b6675b2427185f4b2f85b6c8d8f877c2e573419b
azaw502 <andrew502502@gmail.com>
2019-11-25 02:22:38 -0500

    reformatted api for songs

--- frontend/src/scenes/songs/Songs.js
+++ frontend/src/scenes/songs/Songs.js
@@ -10,21 +10,24 @@ class Songs extends Component {
   async componentDidMount() {
     const songs = await this.getSongs();
     this.setState({
-      songs: songs
+      songs: songs[0]
     });
+    console.log(this.state.songs);
   }
 
   async getSongs() {
     const response = await axios.get('/api/songs');
-    console.log(response.data);
     return response.data;
   }
 
   render() {
     return (
       <Container>
-        {this.state.songs.map(song => (
-          <Card>{song.name} by</Card>
+        {this.state.songs.map((song, i) => (
+          <Card key={i}>
+            {song.name} by{' '}
+            {JSON.stringify(song.artists.map(artist => artist.name))}
+          </Card>
         ))}
       </Container>
     );
--- src/routes/api/songs.js
+++ src/routes/api/songs.js
@@ -7,32 +7,44 @@ const { clientSecret } = require('../../../config');
 const redirectUri = 'https://releasem.herokuapp.com/';
 const authOptions = 'sfds';
 
+const getToken = async () => {
+  const response = await axios({
+    url: 'https://accounts.spotify.com/api/token',
+    method: 'post',
+    params: {
+      grant_type: 'client_credentials'
+    },
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    auth: {
+      username: clientId,
+      password: clientSecret
+    }
+  });
+  const token = response.data.access_token;
+  return token;
+};
+
+const getSongs = async (token, artist, offset = 0, amount = 50) => {
+  const url = `https://api.spotify.com/v1/search?q=${artist}&type=track&limit=${amount}&offset=${offset}`;
+  const response = await axios({
+    url,
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+  console.log(response.data.tracks.items);
+  return response.data.tracks.items;
+};
+
 router.get('/', async (req, res) => {
   try {
-    const tokenResponse = await axios({
-      url: 'https://accounts.spotify.com/api/token',
-      method: 'post',
-      params: {
-        grant_type: 'client_credentials'
-      },
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      auth: {
-        username: clientId,
-        password: clientSecret
-      }
-    });
-    const token = tokenResponse.data.access_token;
-    const songSearchResponse = await axios({
-      url: 'https://api.spotify.com/v1/search/?q=drake&type=track',
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    });
-    console.log(songSearchResponse);
-    res.send(songSearchResponse.data.tracks.items);
+    const songs = [];
+    const token = await getToken();
+    songs.push(await getSongs(token, 'drake'));
+    res.send(songs);
   } catch {
     console.log('Could not reach API');
   }
